a9c324e37feccd1c7caa998d8947b66a18f43ca3
Samppa-code <60503282+Samppa-code@users.noreply.github.com>
2021-08-26 14:53:06 +0300

    Lisätty uusia funktioita

--- hetutarkistus.py
+++ hetutarkistus.py
@@ -1,4 +1,15 @@
-    # HENKILÖTUNNUKSEN TARKISTUSSOVELLUS  130728-478n
+# HENKILÖTUNNUKSEN TARKISTUSSOVELLUS  130728-478n
+
+# Globaalit muuttujat
+import datetime
+nyt = datetime.datetime.now()
+
+# Sanakirja, jossa vuosisatakoodit 
+vuosisadat = {"+": 1800, "-": 1900, "A": 200}
+
+# Sanakirja, jossa jakojäännösten koodit
+tarkisteet = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "A", 11: "B", 12: "C", 13: "D", 14: "E",
+                    15: "F", 16: "H", 17: "J", 18: "K", 19: "L", 20: "M", 21: "N", 22: "P", 23: "R", 24: "S", 25: "T", 26: "U", 27: "V", 28: "W", 29: "X", 30: "Y"}
 
 def tarkista_hetu(hetu):
     """[Tarkistaa, että suomalainen henkilötunnus on muodostettu oikein
@@ -13,13 +24,6 @@ def tarkista_hetu(hetu):
     
     # Muutetaan hekilötunnus isoihin kirjaimiin
     hetu = hetu.upper()
-
-    # Sanakirja vuosisatakoodin selvittämiseen
-    vuosisadat = {"+": 1800, "-": 1900, "A": 200}
-
-    # Sanakirja tarkisteiden hakemiseen
-    tarkisteet = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "A", 11: "B", 12: "C", 13: "D", 14: "E",
-                    15: "F", 16: "H", 17: "J", 18: "K", 19: "L", 20: "M", 21: "N", 22: "P", 23: "R", 24: "S", 25: "T", 26: "U", 27: "V", 28: "W", 29: "X", 30: "Y"}
 
     # Erotetaan päivä omaan muuttujaan
     paivat_str = hetu[0] + hetu[1]
@@ -37,11 +41,9 @@ def tarkista_hetu(hetu):
 
     # Lasketaan jakojäännös 31:llä jaettuna (130728478 % 31) eli modulo 31
     jakojaannos = isoluku % 31
-    # print("Jakojäännös ", jakojaannos)
 
     # Haetaan jakojöönnöstä vastaava kirjain
     uudelleenlaskettu_tarkiste = tarkisteet[jakojaannos]
-    # print("uusi tarkiste", uudelleenlaskettu_tarkiste)
 
     # Tarkistetaan täsmääkö tarkiste
     if uudelleenlaskettu_tarkiste == tarkiste_str:
@@ -51,3 +53,101 @@ def tarkista_hetu(hetu):
         oikein = False
         
     return oikein
+
+def tarkista_pituus(hetu):
+    """Tarkistaa henkilötunnuksen pituuden po. 11 merkkiä
+
+    Args:
+        hetu (string): Henkilötunnus
+
+    Returns:
+        boolean: True, jos 11 merkkiä, False, jos jotain muuta
+    """
+    pituus = len(hetu)
+
+    if pituus == 11:
+        pituus_ok = True
+    else:
+        pituus_ok = False
+    return pituus_ok
+
+def selvita_sukupuoli(hetu):
+    """Selvittää järjestysnumeron perusteella sukupuolen: parillinen -> nainen, pariton, -> mies
+
+    Args:
+        hetu (string): Henkilötunnus
+
+    Returns:
+        string: Nainen tai mies
+    """
+    # Otetaan hetusta järjestysnumero-osa
+    jarjestysnumero_str = hetu[7:10]
+
+    # Muutetaan järjestysnumero luvuksi
+    jarjestysnumero = int(jarjestysnumero_str)
+
+    # Lasketaan modulo 2
+    if jarjestysnumero % 2 == 0:
+        sukupuoli = "Nainen"
+    if jarjestysnumero % 2 !=0:
+        sukupuoli = "Mies"
+    return sukupuoli
+
+def syntymapaiva(hetu):
+    """[summary]
+
+    Args:
+        hetu (string): Henkilötunnus
+
+    Returns:
+        [string]: Laskettu syntymäpäivä
+    """
+    paivat_str = hetu[0] + hetu[1]
+    kuukaudet_str = hetu[2:4]
+    vuodet_str = hetu[4:6]
+    vuosisatakoodi_str = hetu[6]
+
+    # Haetaan vuosisata sanakirjasta
+    vuosisata = vuosisadat[vuosisatakoodi_str]
+
+    # Yhdistetään Vuosisata ja vuosi
+    syntymavuosi = vuosisata + int(vuodet_str)
+
+    # Muunnetaan syntymävuosi merkkijonoksi
+    syntymävuosi_str = str(syntymavuosi)
+
+    # Yhdistetään päivät, kuukaudet, ja syntymävuosi
+    syntymaaaika = paivat_str + "." + kuukaudet_str + "." + syntymävuosi_str
+    return syntymaaaika
+
+
+def laske_ika(hetu):
+    """Muuttaa syntymäpäivän merkkijonosta datetime muotoon.
+
+    Args:
+        hetu (string): Henkilötunnus
+
+    Returns:
+        datetime: Syntymäpäivä
+    """
+    paivat_str = hetu[0] + hetu[1]
+    kuukaudet_str = hetu[2:4]
+    vuodet_str = hetu[4:6]
+    vuosisatakoodi_str = hetu[6]
+
+    # Haetaan vuosisata sanakirjasta
+    vuosisata = vuosisadat[vuosisatakoodi_str]
+
+    # Yhdistetään Vuosisata ja vuosi
+    syntymavuosi = vuosisata + int(vuodet_str)
+    paivat = int(paivat_str)
+    kuukaudet =int(kuukaudet_str)
+
+    syntymapaiva_datetime = datetime.datetime(syntymavuosi,kuukaudet,paivat)
+
+    paivien_ero = nyt - syntymapaiva_datetime
+
+    return round(paivien_ero.days / 365)
+if __name__ == '__main__':
+    print(nyt)
+    print("Ikä on ", laske_ika("281092-201B"))
--- main.py
+++ main.py
@@ -2,13 +2,39 @@
 
 # Moduulien ja kirjastojen lataikset
 import hetutarkistus as ht
+import datetime
+nyt= datetime.datetime.now()
 
-# Kysytän käyttäjältä henkilötunnus
-kysytty_hetu = input("Anna henkilötunniste ")
-# Tarkistetaan onko hetu oikein
-oli_oikein = ht.tarkista_hetu(kysytty_hetu)
 
-if oli_oikein == True:
-    print("Henkilötunniste oikein!")
-else:
-    print("Henkilötunniste on virheellinen!")
+hetu_jarkevä = False
+
+while  hetu_jarkevä == False:
+    # Kysytän käyttäjältä henkilötunnus
+    kysytty_hetu = input("Anna henkilötunniste ")
+
+    # Tarkistetaan onko hetu oikean pituinen
+    pituus_oikein = ht.tarkista_pituus(kysytty_hetu)
+
+    if pituus_oikein == True:
+     # Tarkistetaan onko hetu oikein
+        try:
+            oli_oikein = ht.tarkista_hetu(kysytty_hetu)
+
+            # Ilmoittaa käyttäjälle oliko hetu oikein
+            if oli_oikein == True:
+                print("Henkilötunniste oikein!")
+                print("Sukupuoli:",ht.selvita_sukupuoli(kysytty_hetu))
+                print("Syntymäpäivä oli", ht.syntymapaiva((kysytty_hetu)))
+                print("Ikä on", ht.laske_ika(kysytty_hetu))
+                
+                hetu_jarkevä = True
+        
+            else:
+                print("Henkilötunniste on virheellinen!")
+                hetu_jarkevä = False
+        except:
+            print("Tapahtui virhe")
+            hetu_jarkevä= False
+    else:
+        print("Henkilötunnuksen pituus väärä")
+        hetu_jarkevä = False
